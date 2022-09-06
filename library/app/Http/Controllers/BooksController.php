f863dd269b750e041102349bcf4aa4dbeaa8613a
raadkasem <rkasem0@gmail.com>
2022-09-06 12:40:59 +0300

    Add books resources

---
+++ library/app/Http/Controllers/BooksController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BooksController extends Controller
+{
+
+    public function store()
+    {
+        Book::create($this->validate_request());
+    }
+
+    public function update(Book $book)
+    {
+        $book->update($this->validate_request());
+    }
+
+    private function validate_request()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required'
+        ]);
+    }
+}
---
+++ library/tests/Feature/BookReservationTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Book;
+
+class BookReservationTest extends TestCase
+{
+
+    use RefreshDatabase;
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Raad'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        // $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Raad'
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+    /** @test */
+    public function an_author_is_required()
+    {
+        // $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => ''
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /**
+     * 
+     * @test
+     */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Cool update',
+            'author' => 'Raad update'
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/'.$book->id, [
+            'title' => 'New title',
+            'author' => 'New author'
+        ]);
+        $this->assertEquals('New title', Book::first()->title);
+        $this->assertEquals('New author', Book::first()->author);
+    }
+}
