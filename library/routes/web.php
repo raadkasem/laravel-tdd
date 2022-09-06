8732fe4deb2913c9ee53293795323fd7326124ab
raadkasem <rkasem0@gmail.com>
2022-09-06 12:41:14 +0300

    updating web routes

--- library/routes/web.php
+++ library/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/books', 'App\Http\Controllers\BooksController@store');
+Route::patch('/books/{book}', 'App\Http\Controllers\BooksController@update');
