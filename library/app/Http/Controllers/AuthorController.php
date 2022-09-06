f31cb4f7335830a6c3b6082a2463d3a989652c01
raadkasem <rkasem0@gmail.com>
2022-09-06 16:26:28 +0300

    Author controller create

--- library/app/Http/Controllers/AuthorController.php
+++ library/app/Http/Controllers/AuthorController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Author;
 
 class AuthorController extends Controller
 {
-    //
+    public function store()
+    {
+        $data = request()->only([
+            'name',
+            'dob'
+        ]);
+
+        Author::create($data);
+    }
+
+    // public function update(Author $author)
+    // {
+    //     $author->update();
+    // }
 }
