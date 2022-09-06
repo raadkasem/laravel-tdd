c3f4a31b5530c5c24955abc387fc463ab5f904c4
raadkasem <rkasem0@gmail.com>
2022-09-06 16:27:27 +0300

    Author create test

--- library/tests/Feature/AuthorManagementTest.php
+++ library/tests/Feature/AuthorManagementTest.php
@@ -5,17 +5,22 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use App\Models\Author;
+use Carbon\Carbon;
 class AuthorManagementTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test */
     public function an_author_can_be_created()
     {
-        $response = $this->post('/author',[
+        $this->withoutExceptionHandling();
+        $this->post('/author', [
             'name' => 'Autor Name',
             'dob' => '05/14/1998'
         ]);
-
-        $response->assertCount(1, Author::all());
+        $author = Author::all();
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class , $author->first()->dob);
+        $this->assertEquals('1998/14/05', $author->first()->dob->format('Y/d/m'));
     }
 }
